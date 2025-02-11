b9fb9f9ee6cf96f885400ccc973dd70c7ab0e727
Estefania-2023 <estefa20or@gmail.com>
2025-02-11 18:31:15 -0500

    practice wdd330

---
+++ scr/js/router.js
@@ -0,0 +1,37 @@
+import { createElement } from './utils.js';
+import HomeView from './HomeView.js';
+import FavoriteRecipesView from './FavoritesView.js';
+import RecipeView from './RecipesView.js';
+
+export function initRouter(mainView) {
+  function updateView(newView) {
+    mainView.innerHTML = '';
+    mainView.appendChild(newView);
+  }
+
+  function hashToRoute(hash) {
+    switch (hash) {
+      case '#/home':
+        updateView(HomeView());
+        break;
+      case '#/recipes':
+        updateView(RecipeView());
+        break;
+      case '#/favoriterecipes':
+        updateView(FavoriteRecipesView());
+        break;
+      default:
+        updateView(createElement('h3', { textContent: '404 Page Not Found' }));
+        break;
+    }
+  }
+
+  const defaultHash = window.location.hash || '#/home';
+  hashToRoute(defaultHash);
+
+  window.addEventListener('hashchange', (evt) => {
+    const newUrl = new URL(evt.newURL);
+    const hash = newUrl.hash;
+    hashToRoute(hash);
+  });
+}
